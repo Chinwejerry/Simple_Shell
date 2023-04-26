6f35ea21529b6f88533ddcb02e1c7467391f94bb
Chinwejerry <okekefaithchinwe@gmail.com>
2023-04-26 14:53:24 -0700

    simple shell

--- check_syntax_error.c
+++ check_syntax_error.c
@@ -103,30 +103,30 @@ int first_char(char *input, int *i)
  */
 void print_syntax_error(data_shell *datash, char *input, int i, int bool)
 {
-	char *msg, *msg2, *msg3, *error, *counter;
-	int length;
+	char *message, *message2, *message3, *error, *counter;
+	int scope;
 
 	if (input[i] == ';')
 	{
 		if (bool == 0)
-			msg = (input[i + 1] == ';' ? ";;" : ";");
+			message = (input[i + 1] == ';' ? ";;" : ";");
 		else
-			msg = (input[i - 1] == ';' ? ";;" : ";");
+			message = (input[i - 1] == ';' ? ";;" : ";");
 	}
 
 	if (input[i] == '|')
-		msg = (input[i + 1] == '|' ? "||" : "|");
+		message = (input[i + 1] == '|' ? "||" : "|");
 
 	if (input[i] == '&')
-		msg = (input[i + 1] == '&' ? "&&" : "&");
+		message = (input[i + 1] == '&' ? "&&" : "&");
 
-	msg2 = ": Syntax error: \"";
-	msg3 = "\" unexpected\n";
+	message2 = ": Syntax error: \"";
+	message3 = "\" unexpected\n";
 	counter = aux_itoa(datash->counter);
-	length = _strlen(datash->av[0]) + _strlen(counter);
-	length += _strlen(msg) + _strlen(msg2) + _strlen(msg3) + 2;
+	scope = _strscp(datash->av[0]) + _strscp(counter);
+	scope += _strscp(message) + _strscp(message2) + _strscp(message3) + 2;
 
-	error = malloc(sizeof(char) * (length + 1));
+	error = malloc(sizeof(char) * (scope + 1));
 	if (error == 0)
 	{
 		free(counter);
@@ -135,12 +135,12 @@ void print_syntax_error(data_shell *datash, char *input, int i, int bool)
 	_strcpy(error, datash->av[0]);
 	_strcat(error, ": ");
 	_strcat(error, counter);
-	_strcat(error, msg2);
-	_strcat(error, msg);
-	_strcat(error, msg3);
+	_strcat(error, message2);
+	_strcat(error, message);
+	_strcat(error, message3);
 	_strcat(error, "\0");
 
-	write(STDERR_FILENO, error, length);
+	write(STDERR_FILENO, error, scope);
 	free(error);
 	free(counter);
 }
--- cmd_exec.c
+++ cmd_exec.c
@@ -32,14 +32,14 @@ int is_cdir(char *path, int *i)
 char *_which(char *cmd, char **_environ)
 {
 	char *path, *ptr_path, *token_path, *dir;
-	int len_dir, len_cmd, i;
+	int scp_dir, scp_cmd, i;
 	struct stat st;
 
 	path = _getenv("PATH", _environ);
 	if (path)
 	{
 		ptr_path = _strdup(path);
-		len_cmd = _strlen(cmd);
+		scp_cmd = _strscp(cmd);
 		token_path = _strtok(ptr_path, ":");
 		i = 0;
 		while (token_path != NULL)
@@ -47,8 +47,8 @@ char *_which(char *cmd, char **_environ)
 			if (is_cdir(path, &i))
 				if (stat(cmd, &st) == 0)
 					return (cmd);
-			len_dir = _strlen(token_path);
-			dir = malloc(len_dir + len_cmd + 2);
+			scp_dir = _strscp(token_path);
+			dir = malloc(scp_dir + scp_cmd + 2);
 			_strcpy(dir, token_path);
 			_strcat(dir, "/");
 			_strcat(dir, cmd);
